0889ae15fb8ab742c1139ab8c40a0e19ac2f4bf0
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-17 02:11:31 +0530

    Finds average of two numbers in python

---
+++ Python Programming /Lecture 1 /practice 3.py
@@ -0,0 +1,5 @@
+# finds the average oif two numbers 
+
+num1=int(input("Enter 1st number :-"))
+num2=int(input("Enter 2st number :-"))
+print("The average of above two numbers is :- ", (num1+num2)/2)
