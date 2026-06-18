94807bdd447f2ef686cce675f523f3f00227aad6
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-19 01:06:06 +0530

    program to find the multiple of given number

---
+++ Python Programming /Lecture 2/practice 5.py
@@ -0,0 +1,7 @@
+# WAP to check the given number is multipl₹e of 7 or not 
+
+int=int(input("Enter a number :-"))
+if(int%7==0):
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
