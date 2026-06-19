cba9d045c085820a6575e43ab43203a9391264ac
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-19 23:38:24 +0530

    Program to count the given element in list

---
+++ Python Programming /Lecture 3 /practice 3.py
@@ -0,0 +1,11 @@
+# WAP to count the number of students with the same grade 
+
+tup=("A","D","A","B","C","D","A","D","A","A","W","D","C","D","S","S")
+
+print(tup.count("A"))
+print(tup.count("B"))
+print(tup.count("C"))
+print(tup.count("D"))
+print(tup.count("S"))
+print(tup.count("W"))
+
