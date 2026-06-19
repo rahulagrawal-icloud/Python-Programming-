af06c96df20ba90663d8c38e06c65bee33c6c413
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-19 23:39:12 +0530

    Program to take data from user and store in list

---
+++ Python Programming /Lecture 3 /practice 1.py
@@ -0,0 +1,9 @@
+# WAP to take data from user and store in list 
+
+str=input("Enter the name of 1st Movie :-")
+str2=input("Enter the name of 2st Movie :-")
+str3=input("Enter the name of 3st Movie :-")
+
+list=[str,str2,str3]
+
+print(list)
