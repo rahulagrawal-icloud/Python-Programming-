516915cd1d7ac1d2f4e8bb42454ff35eb70f01ac
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-23 01:03:41 +0530

    Program to store data in dict

---
+++ Python Programming /Lecture 4/practice 1.py
@@ -0,0 +1,9 @@
+# WAP to store the following in dictionary
+dict1 = {"table" : ["a piece of furniture" , "List of facts & figure"]}
+dict2 = {"cat" : "a small Animal"}
+
+# WAP to count how many classrooms are needed for all subjects 
+
+subject = {"python" , "java" , "c++" , "python" , "javascript" , "java" , "python" , "java" , "c++" , "c"}
+
+print(len(subject))
