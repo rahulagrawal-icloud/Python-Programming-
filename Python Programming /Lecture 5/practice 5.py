5a1374fab111b0fd7c2b7fbaee2bbd18b9166fb7
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-29 01:06:40 +0530

    WAP to search given number in loop

---
+++ Python Programming /Lecture 5/practice 5.py
@@ -0,0 +1,11 @@
+# WAP to search given number in loop 
+t = (1,4,9,16,25,36,49,64,81,100)
+x=49
+i=0
+while i<len(t):
+    if(t[i] == x):
+        print("Found at index....",i)
+    
+    else:
+        print("Founding......")
+    i+=1
