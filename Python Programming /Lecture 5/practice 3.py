9f33bbca7684920c26ef1007a52dfc274cc96ee9
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-29 01:06:53 +0530

    WAP to print table of number given by User

---
+++ Python Programming /Lecture 5/practice 3.py
@@ -0,0 +1,10 @@
+# WAP to print table of number given by User
+
+i=int(input("Enter number to print table :-"))
+count = 1
+
+while count<=10 :
+   print(i , "X" , count , "=" , count*i)
+   count+=1
+   
+            
