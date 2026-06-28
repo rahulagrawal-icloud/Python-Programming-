f9b86be0cef4b454b7d83bde27f20ad0ecf7692f
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-29 01:06:29 +0530

    WAP to search any number in loop using for loops

---
+++ Python Programming /Lecture 5/practice 7.py
@@ -0,0 +1,10 @@
+# WAP to search any number in loop using for loops 
+
+l = [1,4,9,16,25,36,49,64,81,100]
+x=36
+for i in l:
+    if(i == x):
+        print("Founded",i)
+    else:
+        print("Founding......")
+
