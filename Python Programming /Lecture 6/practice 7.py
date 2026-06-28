c6d8cdd923dd041ae22b6b561986fa9d308f6fcc
Rahul Agrawal <work.rahulagrawal@gmail.com>
2026-06-29 01:08:19 +0530

    WAP to take number from user and find even or odd using function

---
+++ Python Programming /Lecture 6/practice 7.py
@@ -0,0 +1,13 @@
+# WAP to take number from user and find even or odd using function
+
+def even_odd(a):
+    a=int(input("Enter the number :- "))
+
+    if(a%2==0):
+        print("EVEN")
+    else:
+        print("ODD")
+    
+
+
+even_odd(11)
